44b5937ea701655fa41871811303cfac2e2d7260
Aleksander <smolowik@gmail.com>
2016-05-18 08:13:30 +0200

    Update objectTranslatedColumnGetter.php

--- src/templates/objectTranslatedColumnGetter.php
+++ src/templates/objectTranslatedColumnGetter.php
@@ -1,6 +1,13 @@
 
 <?php echo $comment ?>
-<?php echo $functionStatement ?>
+<?php echo $comment ?>
+
+<?php echo $column->getAccessorVisibility() ?> function get<?php echo $columnPhpName?>($locale = null<?php 
+if ($column->isLazyLoad()) {
+ 	echo ", ConnectionInterface \$con = null";
+}
+?>)
+{
 
     return $this->getCurrentTranslation()->get<?php echo $columnPhpName ?>(<?php echo $params ?>);
 }
